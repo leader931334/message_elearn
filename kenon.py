2d1ef595e62c15aef2e38211d7b69ca6270348ca
きでぃ <srpkdyy@gmail.com>
2020-12-13 04:23:21 +0900

    Update kenon.py

--- kenon.py
+++ kenon.py
@@ -52,7 +52,7 @@ def kenon_submit(session, idx):
 
 
 if __name__ == '__main__':
-    time.sleep(0 * random.randint(0, 29))
+    time.sleep(60 * random.randint(0, 29))
     with requests.Session() as ss:
         login(ss)
 
